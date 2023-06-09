fb103d4fd5b4e393e34d771eac3ba7f630fb7556
Samuel Urom <urom.samuel@gmail.com>
2023-06-09 16:54:51 +1000

    refactored apps

--- category/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Category, CategorizedItem
-
-
-class categoryForm(forms.ModelForm):
-    class Meta:
-        model = Category
-        fields = "__all__"
--- tag/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Tag, TaggedItem
-
-
-class TagForm(forms.ModelForm):
-    class Meta:
-        model = Tag
-        fields = "__all__"
