871960728c85f881a41a6d9c77e54eb6fa2a73ed
Samuel Urom <urom.samuel@gmail.com>
2023-06-09 23:00:12 +1000

    Registered models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag, CategorizedPost
+from .models import Post, Category, Tag
 
 # Register your models here.
 admin.site.register(Post)
