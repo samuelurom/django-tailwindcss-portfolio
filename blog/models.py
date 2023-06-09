1ce2957ac41e50d0f003dbcac6c8a39a58ce7f13
Samuel Urom <urom.samuel@gmail.com>
2023-06-09 20:32:47 +1000

    Added ImageField to feature_image and default slugs on save()

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -11,10 +12,10 @@ class Post(models.Model):
     POST_STATUS = [(PUBLISH, "Publish"), (DRAFT, "Draft")]
 
     title = models.CharField(max_length=255, db_index=True, unique=True)
-    slug = models.SlugField(max_length=255, unique=True)
-    description = models.TextField()
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     content = models.TextField()
-    featured_image = models.CharField(max_length=255)
+    featured_image = models.ImageField(max_length=255, null=True, blank=True)
     status = models.CharField(max_length=1, choices=POST_STATUS, default=DRAFT)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
@@ -22,16 +23,36 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        """Override the save method"""
+        # If slug is not set, generate from title
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
 
 class Category(models.Model):
     """Model for categories"""
 
     title = models.CharField(max_length=80, unique=True)
-    slug = models.SlugField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=80, unique=True, blank=True)
     description = models.TextField()
+
+    class Meta:
+        # Customize the display name in plural
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        """Override save() method"""
+        # If slug is not set, generate from title
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
 
 
 class CategorizedPost(models.Model):
@@ -50,6 +71,13 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        """Override the save() method"""
+        # If slug is not set, generate from title
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 
 class TaggedPost(models.Model):
