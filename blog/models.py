3039262092319f3d0666fb7d3d827b656b99afcf
Samuel Urom <urom.samuel@gmail.com>
2023-06-09 21:57:05 +1000

    Added scheduling and publish date

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,29 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils import timezone
 
 
 # Create your models here.
 class Post(models.Model):
     """Model for posts"""
 
-    DRAFT = 0
-    PUBLISH = 1
+    DRAFT = "draft"
+    SCHEDULE = "schedule"
+    PUBLISH = "publish"
 
-    POST_STATUS = [(PUBLISH, "Publish"), (DRAFT, "Draft")]
+    POST_STATUS_CHOICES = [
+        (DRAFT, "Draft"),
+        (SCHEDULE, "Schedule"),
+        (PUBLISH, "Publish"),
+    ]
 
     title = models.CharField(max_length=255, db_index=True, unique=True)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    content = models.TextField()
+    body = models.TextField()
     featured_image = models.ImageField(max_length=255, null=True, blank=True)
-    status = models.CharField(max_length=1, choices=POST_STATUS, default=DRAFT)
+    status = models.CharField(max_length=8, choices=POST_STATUS_CHOICES, default=DRAFT)
+    publish_date = models.DateTimeField(blank=True, null=True, default=None)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
@@ -25,10 +32,22 @@ class Post(models.Model):
 
     def save(self, *args, **kwargs):
         """Override the save method"""
+
         # If slug is not set, generate from title
         if not self.slug:
             self.slug = slugify(self.title)
 
+        # Check if post status is set to publish and update publish_date
+        if self.status == self.PUBLISH and not self.publish_date:
+            self.publish_date = timezone.now()
+        elif self.status == self.DRAFT and self.publish_date:
+            self.publish_date = None
+        elif self.publish_date > timezone.now():
+            self.status = self.SCHEDULE
+        elif self.status == self.SCHEDULE and self.publish_date <= timezone.now():
+            self.status = self.PUBLISH
+
+        # save the post
         super().save(*args, **kwargs)
 
 
