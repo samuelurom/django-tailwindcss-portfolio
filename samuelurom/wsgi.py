abde2c7e84b85002e71ca37be85e303a2b553dcc
Samuel Urom <urom.samuel@gmail.com>
2023-06-18 14:57:50 +1000

    Initial commit

--- samuelurom/wsgi.py
+++ samuelurom/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samuelurom.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "samuelurom.settings")
 
 application = get_wsgi_application()
