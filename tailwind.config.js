db82160abe67334c66290b43c5443ef7a2608011
Samuel Urom <urom.samuel@gmail.com>
2023-06-18 14:31:48 +1000

    Set up tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.{html,js}", "./static/js/*.js"],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+}
+
