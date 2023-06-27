a37d7ea133d170c8d8eb9f9ce6c42c4ab5f485a4
Samuel Urom <urom.samuel@gmail.com>
2023-06-27 16:22:13 +1000

    Updated settings for deployment on Vercel

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -128,11 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = "static/"
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.joing(BASE_DIR, "staticfiles_build", "static")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 # Media files
 MEDIA_URL = "media/"
