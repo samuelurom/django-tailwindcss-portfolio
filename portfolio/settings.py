6814f9dac26a25d8c212f4c890d9a33df7a5facb
Samuel Urom <urom.samuel@gmail.com>
2023-06-27 14:21:10 +1000

    Updated allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
