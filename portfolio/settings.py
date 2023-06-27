c43774dfd6a1e8f20eab6630db7594d0c99a61d2
Samuel Urom <urom.samuel@gmail.com>
2023-06-27 13:37:53 +1000

    Settings of PostgresSQL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql" or "django.db.backends.sqlite3",
+        "NAME": config("POSTGRES_NAME") or BASE_DIR / "db.sqlite3",
+        "URL": config("POSTGRES_URL"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
     }
 }
 
