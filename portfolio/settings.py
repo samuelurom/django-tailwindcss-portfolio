92e4ed89e5756bb316c4e8ffff86f9caae9577bb
Samuel Urom <urom.samuel@gmail.com>
2023-06-28 19:19:04 +1000

    Updated static url config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -157,7 +157,7 @@ AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
 AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
-# AWS_DEFAULT_ACL = "public-read"
+AWS_DEFAULT_ACL = "public-read"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
