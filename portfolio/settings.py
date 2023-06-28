edcd5686ca8fe747cd77af92085afe4d2a5f6f2d
Samuel Urom <urom.samuel@gmail.com>
2023-06-28 19:29:15 +1000

    Disabled AWS S3 default ACL config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -157,7 +157,7 @@ AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
 AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
-AWS_DEFAULT_ACL = "public-read"
+# AWS_DEFAULT_ACL = "public-read"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
